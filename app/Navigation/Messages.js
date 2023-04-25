0a3485d76e8fa33b833045bba254ce3a48e28852
Brandon Woods <woods.brandon.m@gmail.com>
2023-04-25 16:34:42 -0700

    add Provider to all components

--- app/Navigation/Messages.js
+++ app/Navigation/Messages.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
+
+import store from '../Redux/Store';
 import MessagesMain from '../Screens/message/index';
 
 function Messages() {
-  return <MessagesMain />;
+  return (
+    <Provider store={store}>
+      <MessagesMain />
+    </Provider>
+  );
 }
 
 export default Messages;
