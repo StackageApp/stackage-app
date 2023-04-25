0fd08d6345bf42b6152912b80aacf6030372053d
Jenessa Peterson <jenessa.peterson@gmail.com>
2023-04-25 12:15:30 -0700

    file created

---
+++ app/Screens/makepost/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  Image,
+  Keyboard,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+  enablesReturnKeyAutomatically,
+} from 'react-native';
+
+const localStyles = StyleSheet.create({
+  container: {
+    alignItems: center;
+  }
+});
+
+const MakePost = (props) => {
+  
+
+
+  return <Text>Hello from MakePost!</Text>;
+};
+
+export default MakePost;
