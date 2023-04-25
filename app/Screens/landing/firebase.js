df4f181e545cab8dac320fc6f2aae96dbae33d99
Nam Nguyen <nam.nguyen201@gmail.com>
2023-04-25 16:43:58 -0700

    create firebase file

---
+++ app/Screens/landing/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyC8cs2mCt6hUzUuLi5klg3rHKYEHAtzThM',
+  authDomain: 'stackage-a022a.firebaseapp.com',
+  projectId: 'stackage-a022a',
+  storageBucket: 'stackage-a022a.appspot.com',
+  messagingSenderId: '460753764841',
+  appId: '1:460753764841:web:b2bd079d31d51d31dc0b78',
+  measurementId: 'G-Q25VCYP0D7',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirebase(app);
+
