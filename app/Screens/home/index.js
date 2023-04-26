d0c5083a11bd2a0b823922f50b42942c73a2db33
Brandon Woods <woods.brandon.m@gmail.com>
2023-04-25 23:36:35 -0700

    update home comments

--- app/Screens/home/index.js
+++ app/Screens/home/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Button, Text, View } from 'react-native';
 
+// DISPATCH TO CHANGE STATE, SELECTOR TO GET STATE
 import { useDispatch, useSelector } from 'react-redux';
 
-// DISPATCH TO CHANGE STATE, SELECTOR TO GET STATE
 // IMPORT REDUCER METHODS TO UPDATE STATE
 import { returnTwo } from '../../Redux/Slices/homeSlice';
 import Post from '../../SharedComponents/Post';
 
 function Home() {
   // useSelector to return state goes to Store -> reducer method -> data
-  const title = useSelector((store) => store.homeFeed.posts[0].uid);
-  const dispatch = useDispatch();
+  // const title = useSelector((store) => store.homeFeed.posts[0].uid);
+  // const dispatch = useDispatch();
 
   return (
     <View>
-      <Text>{title}</Text>
+      {/* <Text>{title}</Text> */}
       {/* PASS REDUCER METHOD IN TO DISPATCH WITH ARGUMENTS TO CHANGE STATE */}
       {/* <Button onPress={() => dispatch(returnTwo('003'))}>Click here</Button> */}
       <Post />
