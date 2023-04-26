60d853c82ca2cb3cec3c8d82a270203124c09e08
Kyle Stevens <kyleastevens3@gmail.com>
2023-04-25 19:17:50 -0700

    working on router

--- app/Screens/profile/_layout.js
+++ app/Screens/profile/_layout.js
@@ -24,3 +24,24 @@ export default function Layout() {
     </Stack>
   );
 }
+
+// export default function myStack() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen
+//         name="Profile"
+//         options={{
+//           // title: 'My Profile',
+//           headerShown: false
+//         }}
+//       />
+//       <Stack.Screen
+//         name="Settings"
+//         options={{
+//           title: 'Settings',
+//           gestureEnabled: false,
+//         }}
+//       />
+//     </Stack.Navigator>
+//   )
+// }
--- app/Screens/profile/index.js
+++ app/Screens/profile/index.js
@@ -26,24 +26,3 @@ const styles = StyleSheet.create({
     padding: 10,
   },
 });
-
-// export default function myStack() {
-//   return (
-//     <Stack.Navigator>
-//       <Stack.Screen
-//         name="Profile"
-//         options={{
-//           // title: 'My Profile',
-//           headerShown: false
-//         }}
-//       />
-//       <Stack.Screen
-//         name="Settings"
-//         options={{
-//           title: 'Settings',
-//           gestureEnabled: false,
-//         }}
-//       />
-//     </Stack.Navigator>
-//   )
-// }
