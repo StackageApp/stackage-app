2efc471608e793d873ff940cafee6cd06c5ccb7d
Donn Neufeld <donn.neufeld@gmail.com>
2023-04-26 15:35:00 -0700

    Approvals Count

--- app/Screens/approvals/ApprovalsFeed.js
+++ app/Screens/approvals/ApprovalsFeed.js
@@ -14,6 +14,7 @@ function ApprovalsFeed() {
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       timestamp: 1569109273726,
       avatar: Mev,
+      approvedCount: 0,
     },
     {
       id: '2',
@@ -21,6 +22,7 @@ function ApprovalsFeed() {
       text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       timestamp: 1569109273726,
       avatar: Mev,
+      approvedCount: 2,
     },
     {
       id: '3',
@@ -28,6 +30,7 @@ function ApprovalsFeed() {
       text: 'Amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant.',
       timestamp: 1569109273726,
       avatar: Mev,
+      approvedCount: 4,
     },
     {
       id: '4',
@@ -35,6 +38,7 @@ function ApprovalsFeed() {
       text: 'At varius vel pharetra vel turpis nunc eget lorem. Lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor. Adipiscing tristique risus nec feugiat in fermentum.',
       timestamp: 1569109273726,
       avatar: Mev,
+      approvedCount: 3,
     },
   ];
 
@@ -53,6 +57,9 @@ function ApprovalsFeed() {
             </View>
           </View>
           <Text style={styles.post}>{item.text}</Text>
+        </View>
+        <View>
+          <Text style={styles.approved}>Approval Votes: {item.approvedCount}</Text>
         </View>
       </View>
     );
@@ -107,6 +114,15 @@ const styles = StyleSheet.create({
     height: 150,
     borderRadius: 5,
     marginVertical: 16,
+  },
+  approved: {
+    fontSize: 12,
+    color: 'darkgray',
+  },
+  vote: {
+    backgroundColor: 'green',
+    color: '#FFF',
+    borderRadius: 4,
   },
 });
 export default ApprovalsFeed;
