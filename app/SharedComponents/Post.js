2c42c3db72db67b86c095d9d4717e1b0f8020a87
Brandon Woods <woods.brandon.m@gmail.com>
2023-04-25 20:34:23 -0700

    finish post structure

--- app/SharedComponents/Post.js
+++ app/SharedComponents/Post.js
@@ -2,21 +2,32 @@ import React from 'react';
 import { Image, Text, View } from 'react-native';
 
 const dummyPost = {
-  id: 1,
+  uid: 1,
+  approved: true,
+  likes: 5,
   title: 'Redux is hard',
-  textBody: 'redux is tough. This better help or I am going to be mad',
-  name: 'Kyle',
+  text: 'redux is tough. This better help or I am going to be mad',
   category: 'Mobile Development',
   tags: ['why', 'redux', 'help me'],
-  link: ['https://redux.js.org/'],
-  likes: 5,
+  links: [
+    {
+      title: 'redux',
+      url: 'https://redux.js.org/',
+    },
+  ],
   comments: [
-    { name: 'Brandon', text: 'Yeah, redux took awhile to understand!' },
-    { name: 'Mev', text: "I don't know what you're talking about. It's easy!" },
+    {
+      name: 'Brandon',
+      text: 'Yeah, redux took awhile to understand!',
+    },
+    {
+      name: 'Mev',
+      text: "I don't know what you're talking about. It's easy!",
+    },
   ],
 };
 
-export default function Post({ title, textBody, name, category, tags, link, likes, comments }) {
+export default function Post({ title, text, name, category, tags, link, likes, comments }) {
   return (
     <>
       <View className="post-header">
@@ -26,11 +37,13 @@ export default function Post({ title, textBody, name, category, tags, link, like
       </View>
       <View className="post-body">
         <Text>{title}</Text>
-        <Text>{textBody}</Text>
+        <Text>{text}</Text>
         <Text>{link}</Text>
       </View>
       <View className="post-footer">
-        <Text>Tags</Text>
+        {tags.map((tag) => (
+          <Text>{tag} |</Text>
+        ))}
         <Text>
           {likes} Likes {comments.length} Comments
         </Text>
