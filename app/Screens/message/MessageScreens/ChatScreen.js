5fc12b5e2fc522a9f985790d64afdb9fbad06d0f
Majd Saleh <majd.saleh95@gmail.com>
2023-04-27 12:10:53 -0700

    package.json packages

--- app/Screens/message/MessageScreens/ChatScreen.js
+++ app/Screens/message/MessageScreens/ChatScreen.js
@@ -1,6 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
 
+import axios from 'axios';
+import { nanoid } from 'nanoid';
 import { Bubble, GiftedChat, Send } from 'react-native-gifted-chat';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -8,21 +10,46 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function ChatScreen() {
   const [messages, setMessages] = useState([]);
+  const [testMessages, setTestMessages] = useState([]);
+  const messageObj = {
+    _id: 0,
+    text: '',
+    createdAt: new Date(),
+    user: {
+      _id: 0,
+      name: '',
+      avatar: require('../../../../assets/users/Donn.png'),
+    },
+  };
   const { message } = useSelector((state) => state.message);
   console.log('message', message);
+
   useEffect(() => {
+    // axios get request here and assign the value of the response to the testMessages state
+    axios
+      .get('http://127.0.0.1:3000/users/vY1hQh5wpwgI1zzaweeooEqyJAi1')
+      .then((response) => {
+        setTestMessages(response.data);
+        console.log('response', response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    //
+    console.log('testMessages', testMessages);
     setMessages(message);
     // setMessages([
-    //   {
-    //     _id: 3,
-    //     text: 'How are you doing today?',
-    //     createdAt: new Date(),
-    //     user: {
-    //       _id: 2,
-    //       name: 'React Native',
-    //       avatar: require('../../../../assets/users/Donn.png'),
-    //     },
+    // {
+    //   _id: 3,
+    //   text: 'How are you doing today?',
+    //   createdAt: new Date(),
+    //   user: {
+    //     _id: 2,
+    //     name: 'React Native',
+    //     avatar: require('../../../../assets/users/Donn.png'),
     //   },
+    // },
     //   {
     //     _id: 2,
     //     text: 'Hello world',
@@ -57,7 +84,7 @@ function ChatScreen() {
           name="send-circle"
           style={{ marginBottom: 5, marginRight: 5 }}
           size={32}
-          color="#00FF00"
+          color="#1B6B6B"
         />
       </View>
     </Send>
@@ -68,7 +95,7 @@ function ChatScreen() {
       {...props}
       wrapperStyle={{
         right: {
-          backgroundColor: '#4343F1',
+          backgroundColor: '#9ED2C6',
         },
         left: {
           backgroundColor: '#DADDDC',
