895ec2f97f6d40e2263da31109109498a518000c
Nam Nguyen <nam.nguyen201@gmail.com>
2023-04-27 17:47:18 -0700

    change settings to an icon

--- app/Screens/profile/Profile.js
+++ app/Screens/profile/Profile.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import { StyleSheet, View, Pressable, Text } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
 import { Ionicons } from '@expo/vector-icons';
+
+import ProfileFeed from './ProfileFeed';
 // import { Link, useRouter, usePathname } from "expo-router";
 import ProfileInfo from './ProfileInfo';
-import ProfileFeed from './ProfileFeed';
 
 export default function Profile({ navigation }) {
   // const router = useRouter();
   // console.log(usePathname());
   return (
     <View>
-        <Pressable
-          onPress={() => navigation.navigate('Settings')}
-          >
-            <Text>Settings</Text>
-        </Pressable>
-          {/* <Link href='/Settings' asChild> */}
-          {/* {({ hovered, pressed }) => ( */}
-            {/* <Ionicons style={styles.settingsIcon} name="settings-outline" size={36} /> */}
-          {/* )} */}
+      <Pressable onPress={() => navigation.navigate('Settings')}>
+        <Ionicons name="settings-outline" size={30} color="black" />
+      </Pressable>
+      {/* <Link href='/Settings' asChild> */}
+      {/* {({ hovered, pressed }) => ( */}
+      {/* <Ionicons style={styles.settingsIcon} name="settings-outline" size={36} /> */}
+      {/* )} */}
       {/* </Link> */}
       <ProfileInfo />
       <ProfileFeed />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
