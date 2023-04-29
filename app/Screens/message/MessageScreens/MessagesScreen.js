d42d4c36011cb763ec3add24d2409e894879409e
Majd Saleh <majd.saleh95@gmail.com>
2023-04-29 16:11:13 -0700

    moved data to Data.js

--- app/Screens/message/MessageScreens/MessagesScreen.js
+++ app/Screens/message/MessageScreens/MessagesScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 
+import Messages from '../Data';
 import {
   Container,
   MessageText,
@@ -14,58 +15,6 @@ import {
   UserName,
 } from '../MessageStyles/MessageStyles';
 
-const Messages = [
-  {
-    id: '1',
-    userName: 'Brandon',
-    ProfileImg: require('../../../../assets/users/Brandon.png'),
-    messageTime: '4 mins ago',
-    messageText: 'Approvals are looking great',
-  },
-  {
-    id: '2',
-    userName: 'Kyle',
-    ProfileImg: 'https://i.imgur.com/O9IwNcx.png',
-    messageTime: '2 hours ago',
-    messageText: 'I created a new profile recently',
-  },
-  {
-    id: '3',
-    userName: 'Nam',
-    ProfileImg: 'https://i.imgur.com/swnqERH.png',
-    messageTime: '1 hours ago',
-    messageText: 'The design is finished on Figma',
-  },
-  {
-    id: 'pbbQjIfF7abqfvKD38DOBAbgoHr2',
-    userName: 'Mev',
-    ProfileImg: 'https://i.imgur.com/Biw5Jsb.png',
-    messageTime: '1 day ago',
-    messageText: 'hi',
-  },
-  {
-    id: '5',
-    userName: 'Jenessa',
-    ProfileImg: require('../../../../assets/users/Jenessa.png'),
-    messageTime: '2 days ago',
-    messageText: 'I finished setting up the database',
-  },
-  {
-    id: '6',
-    userName: 'Majd',
-    ProfileImg: require('../../../../assets/users/Majd.png'),
-    messageTime: '2 days ago',
-    messageText: 'my code is polished',
-  },
-  {
-    id: '7',
-    userName: 'Donn',
-    ProfileImg: require('../../../../assets/users/Donn.png'),
-    messageTime: '2 days ago',
-    messageText: 'Navigation is good to go',
-  },
-];
-
 function MessagesScreen({ navigation }) {
   return (
     <Container>
@@ -74,8 +23,8 @@ function MessagesScreen({ navigation }) {
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <Tile
-            onPress={
-              () => navigation.navigate('ChatScreen', { userName: item.userName, id: item.id }) // change item.id to _id to match incoming data object properties
+            onPress={() =>
+              navigation.navigate('ChatScreen', { userName: item.userName, id: item.id })
             }
           >
             <UserInfo>
